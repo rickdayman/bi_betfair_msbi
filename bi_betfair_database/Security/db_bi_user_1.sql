d4446c5529c28d6f368b65c578e9a34da59f4704
rickdayman <rickdayman@gmail.com>
2025-08-13 17:17:13 +1200

    Delete bi_betfair_database/Security/db_bi_user_1.sql

--- bi_betfair_database/Security/db_bi_user_1.sql
+++
@@ -1,3 +0,0 @@
-﻿CREATE LOGIN [db_bi_user]
-    WITH PASSWORD = N'uxomxa2tf?bxkZfpute|ravvmsFT7_&#$!~<ggnrw|fbyue=', SID = 0x2208AD0CBF20674DA78FDEDF352C29BD, DEFAULT_DATABASE = [YAHOO], DEFAULT_LANGUAGE = [us_english];
-
